72e31814e32e98b44d27fd76c1aa2d14eca5b1eb
Evgeniy <ska45ska@gmail.com>
2019-11-14 19:56:53 +0200

    Add files via upload

--- dedicorp/js/common.js
+++ dedicorp/js/common.js
@@ -1,5 +1,23 @@
 $(function() {
 
-	// Custom JS
+	$('.selectric').selectric();
 
+	var ru = $("li:contains('RU')");
+	var en = $("li:contains('EN')");
+
+	$("li:contains('RU')").html('RU <img class="flag" src="img/@1x/icon-ru.jpg" alt="">')
+	.addClass('hide-after');
+	$("span:contains('RU')").html('RU <img class="flag-span" src="img/@1x/icon-ru.jpg" alt="">');
+
+	$("li:contains('EN')").html('EN <img class="flag" src="img/@1x/icon-en.jpg" alt="">')
+	.addClass('hide-after');
+	$("span:contains('RU')").html('RU <img class="flag-span" src="img/@1x/icon-ru.jpg" alt="">');
+
+	ru.on('click', function(){
+		$("span:contains('RU')").html('RU <img class="flag-span" src="img/@1x/icon-ru.jpg" alt="">');
+	});
+
+	en.on('click', function(){
+		$("span:contains('EN')").html('EN <img class="flag-span" src="img/@1x/icon-en.jpg" alt="">');
+	});
 });
