f8a0971f4afb43c0a7b1b6a5437100cf82027557
Evgeniy <ska45ska@gmail.com>
2019-11-15 20:04:21 +0200

    Add files via upload

--- dedicorp/js/common.js
+++ dedicorp/js/common.js
@@ -20,4 +20,40 @@ $(function() {
 	en.on('click', function(){
 		$("span:contains('EN')").html('EN <img class="flag-span" src="img/@1x/icon-en.jpg" alt="">');
 	});
+
+	// Slider
+
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		nav: true,
+		navText : [" "," "],
+		dots: true
+		//autoWidth:true
+	});
+
+	// Tabs
+
+$('.tabs__title li').not('.active').click(function(){
+    //номер таба
+    var index = $(this).index();
+    //соответствующая закладка
+    var content = $('.tabs__content-item').eq(index);
+    //таб сделать активным, остальные неактивными
+    $(this).addClass('active').siblings().removeClass('active');
+    //открыть нужную вкладку, закрыть остальные
+    $('.tabs__content-item').css('display', 'none').eq(index).css('display', 'block');
+});
+
+	$('.tabs__title li:first').addClass('active');
+	$('.tabs__content li:first').css('display', 'block');
+
+// Accordeon
+
+$('.accordeon__title').on('click', function(){
+	var next = $(this).next();
+
+	next.slideToggle(400);
+	$('.accordeon__content').not(next).slideUp(400);
+});
+
 });
